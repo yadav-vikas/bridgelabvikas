2572d1c1f23e485459be0433926b6eca82dea652
yadav-vikas <52526748+yadav-vikas@users.noreply.github.com>
2019-08-22 10:45:18 +0530

    Create lexographic.java
    lexographic

---
+++ lexographic.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.functional;
+import java.util.*;
+public class Lexography {
+
+	public static void main(String[] args) {
+		Scanner Scanner=new Scanner(System.in);
+		String Str1=Scanner.nextLine();
+		String Str2=Scanner.nextLine();
+		int maxlength;
+		if(Str1.length()>Str2.length())
+			maxlength=Str1.length();
+		else
+			maxlength=Str2.length();
+		if(Str1.equals(Str2))
+		{
+			System.out.println("0");
+		}
+		for(int i=0;i<maxlength;i++)
+		{
+			
+		}
+
+	}
+
+}
